4a537f9a9b09bab4456b3b378b571930443f5bf9
Sarah Reed Guy <sarahreedguy@gmail.com>
2015-01-27 08:44:50 -0800

    Excercise 3 splits sequence along particualr codon and counts length of each subsequent string

---
+++ excercise_3.py
@@ -0,0 +1,21 @@
+#3
+>>> dna_3="ACTGATCGATTACGTATAGTAGAATTCTATCATACATATATATCGATGCGTTCAT"
+>>> print(str(dna_3.find('GAATTC')))
+#21
+print(dna_3[0:22])
+#ACTGATCGATTACGTATAGTAG
+>>> dna_3p1="ACTGATCGATTACGTATAGTAG"
+>>> print(dna_3[22:100])
+#AATTCTATCATACATATATATCGATGCGTTCAT
+>>> dna_3p2="AATTCTATCATACATATATATCGATGCGTTCAT"
+>>> dna_3p1_length= len(dna_3p1)
+>>> print(str(dna_3p1_length))
+#22
+>>> dna_3p2_length= len(dna_3p2)
+>>> print(str(dna_3p2_length))
+#33
+#Check
+#>>> dna_3="ACTGATCGATTACGTATAGTAGAATTCTATCATACATATATATCGATGCGTTCAT"
+#>>> dna_3l= len(dna_3)
+#>>> print(str(dna_3l))
+##55
